497b20e8a9bd1264116780b69cb1faa686561699
Mobeen Ejaz Ahmad <mobeene98@gmail.com>
2024-06-06 21:41:58 +0500

    chef and intro UI added

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
 
-import './Chef.css';
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .
+          </p>
+        </div>
+        <p className="p__opensans">
+          Veniam labore id occaecat deserunt eiusmod eu nostrud laborum veniam
+          do excepteur eu. Ipsum et proident velit eu velit. Commodo qui anim
+          mollit exercitation sit do cupidatat culpa pariatur veniam qui.
+          Consectetur aliqua eiusmod in deserunt nisi consectetur laboris qui
+          est Lorem.
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
